4bb5c79281941a03c5dc0e77a531b94ff0ae3826
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-07-19 15:25:15 +0900

    Create d.vb

---
+++ 2.vb.net中log处理语句/d.vb
@@ -0,0 +1,15 @@
+Imports System.IO
+一旦添加了这个引入语句，您就可以在 VB 文件中直接使用 File 类来写入 log 文件了。示例代码如下：
+
+Imports System.IO
+
+Module Program
+    Sub Main(args As String())
+        File.AppendAllText("C:\path\to\your\log\file.txt", "This is the value to log.")
+    End Sub
+End Module
+
+
+简单直接引入 Imports System.IO
+然后直接使用下面语句即可输出log
+File.AppendAllText("C:\path\to\your\log\file.txt", "This is the value to log.")
