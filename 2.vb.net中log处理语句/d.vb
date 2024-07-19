eb9d56b39fcdfb4bb30b212831a45aaebbd794ff
mingyifan2023 <150598503+mingyifan2023@users.noreply.github.com>
2024-07-19 16:22:47 +0900

    Update d.vb

--- 2.vb.net中log处理语句/d.vb
+++ 2.vb.net中log处理语句/d.vb
@@ -13,3 +13,22 @@ End Module
 简单直接引入 Imports System.IO
 然后直接使用下面语句即可输出log
 File.AppendAllText("C:\path\to\your\log\file.txt", "This is the value to log." & vbCrLf )
+
+如何用上面的log方法输出一个遍历的值还有这个遍历的类型
+
+
+
+如果您想要将遍历的值和类型输出到日志文件中，您可以使用File.AppendAllText方法和字符串插值（String Interpolation）。以下是一个示例代码：
+
+Imports System.IO
+
+Module Program
+    Sub Main(args As String())
+        Dim logFilePath As String = "C:\path\to\your\log\file.txt"
+        
+        Dim values As Integer() = {1, 2, 3, 4, 5}
+        For Each value In values
+            File.AppendAllText(logFilePath, $"Value: {value}, Type: {value.GetType().Name}" & vbCrLf)
+        Next
+    End Sub
+End Module
